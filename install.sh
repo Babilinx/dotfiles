b8f92897769f1651d01cf61214a13b37c9fe2f80
Babilinx <babilinx.evx1o@simplelogin.com>
2023-11-18 14:32:03 +0100

    feat(install): make installation method more simple
    Now 'install.sh' takes the target in $1

--- install.sh
+++ install.sh
@@ -4,32 +4,20 @@ COLOR_GREEN='\033[0;32m'
 COLOR_RED='\033[0;31m'
 COLOR_RESET='\033[0m'
 
-TARGETS=("intel-laptop-gentoo" "amd-desktop-gentoo")
-
-target_selection() {
-    for (( i = 0; i < ${#TARGETS[@]}; i++ )); do
-        echo "$((i+1)) $TARGETS[i]"
-    done
-    echo
-
-    read -p "Select a target [1-${#TARGETS[@]}]: " TARGET_SELECTED
-    
-    while [[ ! "${TARGETS[$((TARGET_SELECTED+1))]}" ]]; do
-        echo "Wrong input '$TARGET_SELECTED'"
-        read -p "Select a target [1-${#TARGETS[@]}]: " TARGET_SELECTED
-    done
-
-    echo "${TARGETS[$((TARGET_SELECTED+1))]}"
-}
+SELECTED_TARGET=$1
 
 echo "Babilinx's dotfiles installer"
-echo -e "\n\n"
+echo
 
-echo "Select device:"
-echo "Note: If you want to try some things, do the install manually!"
-
-TARGET=$(target_selection)
-echo -e "\ntarget: $TARGET\n"
+case $SELECTED_TARGET in
+    laptop-gentoo-intel) TARGET="laptop-gentoo-intel";;
+    desktop-gentoo-amd) TARGET="desktop-gentoo-amd";;
+    *)
+        echo "'$SELECTED_TARGET' is not a valid target!"
+        echo "Note: If you want to try some things, do the install manually!"
+        echo "(So nothing will be lost)"
+        exit 1;;
+esac
 
 for config_folder in $(ls -d */); do
 	[ "$config_folder" == "img/" ] && continue
