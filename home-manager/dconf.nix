629b06755d7ac3b29028fe1eb0d637f83fce4a8a
babilinx <babilinx.evx1o@simplelogin.com>
2024-06-08 16:50:52 +0200

    feat(hm): add home-manager config

---
+++ home-manager/dconf.nix
@@ -0,0 +1,193 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
+{
+  dconf.settings = {
+    "com/mattjakeman/ExtensionManager" = {
+      last-used-version = "0.5.1";
+    };
+
+    "org/gnome/Console" = {
+      last-window-maximised = false;
+      last-window-size = mkTuple [ 652 480 ];
+      theme = "auto";
+    };
+
+    "org/gnome/Snapshot" = {
+      is-maximized = false;
+      window-height = 640;
+      window-width = 800;
+    };
+
+    "org/gnome/TextEditor" = {
+      indent-style = "space";
+      last-save-directory = "file:///home/bastien/.config/home-manager";
+      show-line-numbers = true;
+      tab-width = mkUint32 2;
+    };
+
+    "org/gnome/baobab/ui" = {
+      is-maximized = false;
+      window-size = mkTuple [ 1430 600 ];
+    };
+
+    "org/gnome/control-center" = {
+      last-panel = "background";
+      window-state = mkTuple [ 980 640 false ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" "Pardus" "951cccf1-7187-46bb-aa74-44f883fda873" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/951cccf1-7187-46bb-aa74-44f883fda873" = {
+      apps = [ "org.gnome.Extensions.desktop" "org.gnome.Settings.desktop" "com.mattjakeman.ExtensionManager.desktop" "xterm.desktop" "cups.desktop" ];
+      name = "Param\233trage";
+      translate = false;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      excluded-apps = [ "org.gnome.Console.desktop" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-l.jxl";
+      picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-d.jxl";
+      primary-color = "#3071AE";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "fr" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "default";
+      gtk-theme = "adw-gtk3";
+    };
+
+    "org/gnome/desktop/notifications" = {
+      application-children = [ "gnome-power-panel" "org-gnome-console" "org-gnome-texteditor" "org-gnome-tweaks" ];
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-console" = {
+      application-id = "org.gnome.Console.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-texteditor" = {
+      application-id = "org.gnome.TextEditor.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-tweaks" = {
+      application-id = "org.gnome.tweaks.desktop";
+    };
+
+    "org/gnome/desktop/privacy" = {
+      old-files-age = mkUint32 30;
+      recent-files-max-age = -1;
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-l.jxl";
+      primary-color = "#3071AE";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 300;
+    };
+
+    "org/gnome/desktop/sound" = {
+      event-sounds = true;
+      theme-name = "__custom";
+    };
+
+    "org/gnome/epiphany" = {
+      ask-for-default = false;
+    };
+
+    "org/gnome/epiphany/web" = {
+      last-upload-directory = "/home/bastien/Images/Captures d\8217\233cran";
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 890 550 ];
+    };
+
+    "org/gnome/portal/filechooser/org/gnome/Epiphany" = {
+      last-folder-path = "/home/bastien/Images/Captures d\8217\233cran";
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      disabled-extensions = [];
+      enabled-extensions = [ "luminus-shell@dikasp.gitlab" ];
+      favorite-apps = [ "org.gnome.Console.desktop" "firefox.desktop" "librewolf.desktop" "webcord.desktop" ];
+      last-selected-power-profile = "performance";
+      welcome-dialog-last-shown-version = "46.2";
+    };
+
+    "org/gnome/shell/weather" = {
+      automatic-location = true;
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = [];
+    };
+
+    "org/gnome/tweaks" = {
+      show-extensions-notice = false;
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = true;
+      sidebar-width = 140;
+      sort-column = "name";
+      sort-directories-first = true;
+      sort-order = "ascending";
+      type-format = "category";
+      view-type = "list";
+      window-size = mkTuple [ 921 492 ];
+    };
+
+  };
+}
